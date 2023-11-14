af6dd1aa913660a92195e7e4ee014750b46b7790
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-11-14 20:55:40 +0000

    EMpty string returns zero

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void When_String_Is_EMpty_Return_Zero()
+        {
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.Add("");
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+    }
+}
